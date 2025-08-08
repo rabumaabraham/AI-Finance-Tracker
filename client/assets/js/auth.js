970da437ee3d8816fa4a6a9091c88c13445d98f8
rabuma abraham <iamrabuma@gmail.com>
2025-08-09 01:33:54 +0200

    one more bug inside auth.js fixed for vercel deployment

--- client/assets/js/auth.js
+++ client/assets/js/auth.js
@@ -1,7 +1,9 @@
 // Authentication service for connecting frontend to backend
 class AuthService {
     constructor() {
-        this.baseURL = 'http://localhost:5000/api/auth';
+        // this.baseURL = 'http://localhost:5000/api/auth';
+
+        this.baseURL = 'https://finance-tracker-tlss.onrender.com/api/auth';
         this.token = localStorage.getItem('token');
     }
 
